568a956239be6cea76bc350790b63c4f9794ad30
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-04-10 05:43:49 -0400

    add doubleclick:ensure_single_click
    Adds a callback for  when the next doubleclick timer expires

--- .config/awesome/doubleclick.lua
+++ .config/awesome/doubleclick.lua
@@ -6,15 +6,26 @@ function doubleclick:__call()
     if self._timer then
         self._timer:stop()
         self._timer = nil
-        
+        self._single_click_actions = {}
+
         return true
     end
   
-    self._timer = gears.timer.start_new(0.20, function()
+    self._timer = gears.timer.start_new(0.2, function()
         self._timer = nil
+        for _, cb in ipairs(self._single_click_actions) do
+            cb()
+        end
+
         return false
     end)
     return false
 end
 
-return setmetatable({}, doubleclick)
+-- only call the given function when the double click timer expires
+function doubleclick:ensure_single_click(cb)
+    table.insert(self._single_click_actions, cb)
+end
+
+doubleclick.__index = doubleclick
+return setmetatable({ _single_click_actions = {} }, doubleclick)
