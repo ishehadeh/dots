4de89a0922363a2aa12ea39a477fbe7b8ea77a33
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-04-11 06:32:23 -0400

    awesome gruvbox: fix resource paths
    * use theme_path to refer to this theme
    * remove default from hardcoded paths

--- .config/awesome/themes/gruvbox/theme.lua
+++ .config/awesome/themes/gruvbox/theme.lua
@@ -111,7 +111,7 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_submenu_icon = theme_path.."/submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(100)
 
@@ -131,45 +131,45 @@ theme.titlebar_close_button_focus  = theme.titlebar_close_button_normal
 theme.titlebar_minimize_button_normal = circle(palette.yellow)
 theme.titlebar_minimize_button_focus  = theme.titlebar_minimize_button_normal
 
-theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = theme_path.."/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = theme_path.."/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = theme_path.."/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = theme_path.."/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = theme_path.."/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = theme_path.."/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = theme_path.."/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = theme_path.."/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = theme_path.."/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = theme_path.."/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = theme_path.."/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = theme_path.."/titlebar/floating_focus_active.png"
 
 theme.titlebar_maximized_button_normal_inactive = circle(palette.green)
 theme.titlebar_maximized_button_focus_inactive = theme.titlebar_maximized_button_normal_inactive
 theme.titlebar_maximized_button_normal_active = circle(palette.alt.green)
 theme.titlebar_maximized_button_focus_active  = theme.titlebar_maximized_button_normal_active
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = theme_path.."background.png"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.layout_fairh = theme_path.."layouts/fairhw.png"
+theme.layout_fairv = theme_path.."/layouts/fairvw.png"
+theme.layout_floating  = theme_path.."/layouts/floatingw.png"
+theme.layout_magnifier = theme_path.."/layouts/magnifierw.png"
+theme.layout_max = theme_path.."/layouts/maxw.png"
+theme.layout_fullscreen = theme_path.."/layouts/fullscreenw.png"
+theme.layout_tilebottom = theme_path.."/layouts/tilebottomw.png"
+theme.layout_tileleft   = theme_path.."/layouts/tileleftw.png"
+theme.layout_tile = theme_path.."/layouts/tilew.png"
+theme.layout_tiletop = theme_path.."/layouts/tiletopw.png"
+theme.layout_spiral  = theme_path.."/layouts/spiralw.png"
+theme.layout_dwindle = theme_path.."/layouts/dwindlew.png"
+theme.layout_cornernw = theme_path.."/layouts/cornernww.png"
+theme.layout_cornerne = theme_path.."/layouts/cornernew.png"
+theme.layout_cornersw = theme_path.."/layouts/cornersww.png"
+theme.layout_cornerse = theme_path.."/layouts/cornersew.png"
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
