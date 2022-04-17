778180237e603d8d221751314d4d410e0212c8aa
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-04-17 10:05:47 -0400

    activate+raise window when selected with alt-tab

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -332,7 +332,7 @@ awful.keygrabber {
     end,
     stop_callback      = function()
         if switcher.widget:selected() then
-        	switcher.widget:selected():raise()
+        	switcher.widget:selected():emit_signal("request::activate", "mouse_click", {raise = true})
         end
         switcher.widget._private.container:set_children({})
         switcher.visible = false
@@ -726,6 +726,11 @@ client.connect_signal("request::titlebars", function(c)
         layout  = wibox.layout.align.horizontal
     }
 end)
+
+-- Enable sloppy focus, so that focus follows mouse.
+-- client.connect_signal("mouse::enter", function(c)
+--     c:emit_signal("request::activate", "mouse_enter", {raise = false})
+-- end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
