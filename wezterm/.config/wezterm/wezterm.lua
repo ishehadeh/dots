ad775334ade0e64144055ffa9b75f7117209e8d6
Ian Shehadeh <ian@shehadeh.net>
2024-05-22 21:27:02 -0400

    use local multiplexing for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,5 +10,22 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Catppuccin Frappe'
 config.font = wezterm.font 'Iosevka Term'
 
+
+-- configure multiplexing
+config.unix_domains = {
+    {
+        -- The name; must be unique amongst all domains
+        name = 'default',
+
+        -- leaving this blank to use the default
+        -- socket_path = "/some/path",
+
+        -- start the mux server if it's not already running
+        no_serve_automatically = true,
+    },
+}
+
+config.default_gui_startup_args = { 'connect', 'default' }
+
 -- and finally, return the configuration to wezterm
 return config
